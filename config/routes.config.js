2e6d00c63b9ac81ba2a3181d60f5ba2a75def528
Nacho Lorente <ignaciolorentegarcia@gmail.com>
2023-02-14 21:48:42 +0100

    First Views

--- config/routes.config.js
+++ config/routes.config.js
@@ -1,9 +1,15 @@
 const express = require("express");
 
 const common = require("../controllers/common.controller");
+const services = require("../controllers/service.controller");
+const user = require("../controllers/users.controller")
 
 const router = express.Router();
 
 router.get("/", common.home);
+router.get("/services", services.list);
+
+router.get("/users/login", user.login);
+router.get("/users/new", user.create)
 
 module.exports = router;
---
+++ controllers/users.controller.js
@@ -0,0 +1,10 @@
+
+
+
+module.exports.create = (req, res) => {
+  res.render("users/new");
+}
+
+module.exports.login = (req, res) => {
+  res.render("users/login");
+}
